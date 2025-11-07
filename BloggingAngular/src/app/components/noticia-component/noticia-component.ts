b60bfd5cbded2b5a8fe4022319f88f91d8ce353e
P\cloud <pavelmiron6824@gmail.com>
2025-11-07 11:45:52 +0100

    1.0.2. Ajustar el html, formulario izquierda, noticias derecha, componente.ts constructor y push para enviar datos. !ngModel areglar, añadir FormsModule

--- BloggingAngular/src/app/components/noticia-component/noticia-component.ts
+++ BloggingAngular/src/app/components/noticia-component/noticia-component.ts
@@ -8,20 +8,54 @@ import { Noticia } from '../../interface/noticia';
   templateUrl: './noticia-component.html',
   styleUrl: './noticia-component.css',
 })
+
 export class NoticiaComponent {
 
+  // array de noticias y el numero de noticia
   arrayNoticias: Noticia[];
   noticia: number;
+  noticiaActual: Noticia;
+
+  // constructor(){
+  //   this.noticia = 0;
+  //   this.arrayNoticias = [{
+  //     titulo: 'Numero1',
+  //     url: 'https://www.bluehost.com/blog/wp-content/uploads/2023/05/parts-of-a-uniform-resource-locator-URL.png',
+  //     cuerpo: 'aaa',
+  //     fecha: new Date()
+  //   }];
+  // }
 
   constructor(){
-    this.noticia = 0;
-    this.arrayNoticias = [{
-      titulo: 'Numero1',
-      url: 'https://www.bluehost.com/blog/wp-content/uploads/2023/05/parts-of-a-uniform-resource-locator-URL.png',
-      cuerpo: 'aaa',
+
+    this.noticiaActual = {
+      titulo: '',
+      url: '',
+      cuerpo: '',
       fecha: new Date()
-    }];
+    };
+
+    this.noticia = 0;
+
+    this.arrayNoticias = [];
+
   }
+
+  publicarNoticia(){
+    this.noticiaActual.fecha = new Date(this.noticiaActual.fecha);
+
+    this.arrayNoticias.push({...this.noticiaActual});
+
+    //limpiar el fomulario depsues de publicar uno
+    this.noticiaActual = {
+      titulo: '',
+      url: '',
+      cuerpo: '',
+      fecha: new Date()
+    };
+  }
+
+  
 
 }
 
