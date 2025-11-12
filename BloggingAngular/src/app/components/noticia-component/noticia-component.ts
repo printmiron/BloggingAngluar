212ab4413562bd70465623cece76a543bb4f34d6
P\cloud <pavelmiron6824@gmail.com>
2025-11-12 09:24:23 +0100

    2.0.0. Al iniciar el array con dos noticias ya, shadowbox al cards, ajustar margenes, comproar el trabajo y su funcionamiento

--- BloggingAngular/src/app/components/noticia-component/noticia-component.ts
+++ BloggingAngular/src/app/components/noticia-component/noticia-component.ts
@@ -13,25 +13,14 @@ import { CommonModule, DatePipe } from '@angular/common';
 
 export class NoticiaComponent {
 
-  // guardar todas las noticias
+  // guardar todas las noticias en el array, con la estructura de la interfaz
   arrayNoticias: Noticia[];
   // contador de noticias
   noticia: number;
   //noticia que se escribe en el formulario
   noticiaActual: Noticia;
 
-  //ejemplo ar principio
-  // constructor(){
-  //   this.noticia = 0;
-  //   this.arrayNoticias = [{
-  //     titulo: 'Numero1',
-  //     url: 'https://www.bluehost.com/blog/wp-content/uploads/2023/05/parts-of-a-uniform-resource-locator-URL.png',
-  //     cuerpo: 'aaa',
-  //     fecha: new Date()
-  //   }];
-  // }
-
-  constructor(){
+  constructor() {
 
     //inicializa la noticia en blanco y despues inicializa la lista de noticias como array vacio
     this.noticiaActual = {
@@ -41,15 +30,36 @@ export class NoticiaComponent {
       fecha: new Date()
     };
 
+
+
     this.noticia = 0;
 
-    this.arrayNoticias = [];
+    //cargamos en el array dos noticias
+    this.arrayNoticias = [
+
+      {
+        titulo: 'Noticia 1',
+        url: 'https://www.bluehost.com/blog/wp-content/uploads/2023/05/parts-of-a-uniform-resource-locator-URL.png',
+        cuerpo: 'Contenido noticia 1',
+        // mes/dia/año -> no se porque es asi pero en la pagina se ve como -> dia/mes/año
+        fecha: new Date('11/12/2025')
+      },
+      {
+        titulo: 'Noticia 2',
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoDc4YsaPCvNoHh-5x1XkFpGlR7jK12Yt0tg&s',
+        cuerpo: 'Contenido noticia 2',
+        fecha: new Date('11/10/2025')
+      }
+
+    ];
+
+
 
   }
 
-  publicarNoticia(){
+  publicarNoticia() {
     //guarda la noticia en array y para crear una copia y evitar modificar la referencia
-    this.arrayNoticias.push({...this.noticiaActual});
+    this.arrayNoticias.push({ ...this.noticiaActual });
 
     //limpiar el fomulario depsues de publicar uno
     this.noticiaActual = {
@@ -60,8 +70,7 @@ export class NoticiaComponent {
     };
   }
 
-  
+
 
 }
 
-  
